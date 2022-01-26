4a1dfc4ca94e2d7a379432d4938fd0c2e5fb7694
olawale-kareem <olawale.kareem@decagon.dev>
2022-01-26 23:22:28 +0100

    Reinforcement Learning

---
+++ overview.py
@@ -0,0 +1,5 @@
+
+#AI divided into:
+                # ANI: artificial narrow intelligence e.g smart speaker, smart TV, self driving car, web search e.tc
+                # AGI: artificial narrow intelligence; computers behaving like human
+                
